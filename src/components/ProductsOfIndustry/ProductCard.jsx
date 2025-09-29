579b763c17e445718b5643b5391db365f91653a3
Muhammad Hamid Raza <itmughal2023@gmail.com>
2025-09-29 17:16:39 +0500

    fix(ProductCard): update product MOQ and delivery time display

--- src/components/ProductsOfIndustry/ProductCard.jsx
+++ src/components/ProductsOfIndustry/ProductCard.jsx
@@ -8,7 +8,7 @@ export default function ProductCard({ product, parent_name }) {
         <Link href={`/${parent_name}/${product.slug + "-" + product.id}`} className='w-[400px] flex-shrink-0 rounded-lg bg-primary-foreground/80 hover:bg-white hover:shadow-lg p-4'>
             <Image src={product.image} className='rounded-xl' width={400} height={400} alt={product.title} />
             <div className="flex flex-col gap-2 px-2 py-4">
-                <span className='text-sm'>Min. {product.min_units} units · Delivery: {product.delivery}</span>
+                <span className='text-sm'>{product?.moq} • Delivery: {product?.deliveryTime}</span>
                 <span className='font-semibold'>{product.title}</span>
             </div>
             <button className="btn">Request a Quote</button>
